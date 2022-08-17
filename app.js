01034050ad3635a4cbb2e5eb8f0a265205f2e150
yunuswidodo <younews372@gmail.com>
2022-08-17 23:50:30 +0700

    pemanggilan data

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const app = Vue.createApp({
     // data, function
     // template:'<h2>I am a template<h2>'
+    data() {
+        return {
+            title: 'The final Empire',
+            author: 'Brandon Sanderson',
+            age : 45
+        }
+    },
+    methods: {
+        changeTitle(title) {
+            // this.title = 'you click Me';
+            this.title = title
+        }
+    }
+
 })
 
 app.mount('#app');
