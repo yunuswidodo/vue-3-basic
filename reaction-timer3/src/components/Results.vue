fb586a4c40008824b26655e7ccdf9ff939acf9b8
yunuswidodo <younews372@gmail.com>
2022-10-19 06:52:36 +0700

    finish reaction timer

---
+++ reaction-timer3/src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reacion time - {{scorezz}} ms</p>
+  <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+    props: ['scorezz'],
+    data(){
+        return{
+            rank:null
+        }
+    },
+    mounted(){
+        if (this.scorezz < 250) {
+            this.rank = 'Ninja Fingers'
+        }else if(this.scorezz < 400){ 
+            this.rank = 'Rapid Reflexes'
+        }else{
+            this.rank = 'snail pace'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
