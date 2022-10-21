aaaf8628837ad172c7711b5a643270c00e605361
yunuswidodo <younews372@gmail.com>
2022-10-21 07:52:25 +0700

    final form

--- web-form/src/components/signupForm.vue
+++ web-form/src/components/signupForm.vue
@@ -1,47 +1,84 @@
 <template>
-    <form>
+    <form @submit.prevent="handlerSubmit">
         <label>Email</label>
         <input type="email" required v-model="email"> 
+
         <label>Password</label>
         <input type="password" required v-model="password"> 
+        <div v-if="passwordError" class="error"> {{passwordError}}</div>
+
         <label>Role :</label>
         <select v-model="role">
             <option value="developer">web Developer</option>
             <option value="designer">Web Designer</option>
         </select>
+
+        <label>Skills</label>
+        <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+        <div v-for="skill in skills" :key="skill" class="pill">
+            <span @click="deleteSkill(skill)">{{skill}}</span>
+        </div>
+
         <div class="terms">
             <input type="checkbox" v-model="terms" required>
             <label>Accept term and condition</label>
         </div>
-        <div>
-            <input type="checkbox" value="Shaun" v-model="names">
-            <label>Shaun</label>
-        </div>
-        <div>
-            <input type="checkbox" value="Yoshi" v-model="names">
-            <label>Yoshi</label>
-        </div>
-        <div>
-            <input type="checkbox" value="Mario" v-model="names">
-            <label>Mario</label>
+    
+        <div class="submit">
+            <button>Create An Account</button>
         </div>
     </form>
     <p>Email : {{email}}</p>
     <p>Passwor : {{password}}</p>
     <p>Role : {{role}}</p>
     <p>Term Accepted : {{terms}}</p>
-    <p>Names : {{names}}</p>
 </template>
 
 <script>
 export default {
     data(){
         return{
-            email: 'mario',
+            email: '',
             password: '',
             role: 'designer',
             terms: true,
-            names:[]
+            tempSkill: '',
+            skills:[],
+            passwordError: ''
+        }
+    },
+    methods: {
+        addSkill(e){
+            if (e.key === ',' && this.tempSkill){
+                // cek value if same 
+                if(!this.skills.includes(this.tempSkill)){
+                    this.skills.push(this.tempSkill)
+                }
+                this.tempSkill = ''
+            }
+        },
+
+    
+        deleteSkill(skill){
+            // pelajari ini
+            this.skills = this.skills.filter((item)=> {
+                return skill !== item
+            })
+        },
+
+        handlerSubmit(){
+            // console.log('form submited');
+            // validate password
+            this.passwordError = this.password.length > 5 ? 
+            '' : 'password must be at least 6 chars long';
+
+            if (!this.passwordError){
+                console.log('email: ', this.email);
+                console.log('password: ', this.password);
+                console.log('role: ', this.role);
+                console.log('skills: ', this.skills);
+                console.log('term accerpted', this.terms);
+            }
         }
     }
 }
@@ -73,7 +110,6 @@ export default {
         border: none;
         border-bottom: 1px solid #ddd;
         color: #555;
-
     }
 
     input[type="checkbox"]{
@@ -82,6 +118,39 @@ export default {
         margin: 0 10px 0 0;
         position: relative;
         top: 2px;
+    }
+
+    .pill{
+        display: inline-block;
+        margin: 20px 10px 0 0;
+        padding: 6px 12px;
+        background: #eee;
+        border-radius: 20px;
+        font-size: 12px;
+        letter-spacing: 1px;
+        font-weight: bold;
+        color: #777;
+        cursor: pointer;
+    }
+
+    button {
+        background: #0b6dff;
+        border: 0;
+        padding: 10px 20px;
+        margin-top: 20px;
+        color: white;
+        border-radius: 20px;
+    }
+
+    .submit {
+        text-align: center;
+    }
+
+    .error{
+        color: #ff0062;
+        margin-top: 10px;
+        font-size: 0.8em;
+        font-weight: bold;
     }
 </style>
  
